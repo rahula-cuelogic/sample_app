01e435e96d2ae77e39dc30461013286ea7c51eb6
Rahul Agarwal <rahul.agarwal@cuelogic.co.in>
2017-06-20 20:26:39 +0530

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -113,6 +113,17 @@ describe User do
 
 	end
 
+	# 6.5 Exercise
+	#describe "email address with mixed case" do
+	#	let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+	#	it "should be saved as all lower-case" do
+	#		@user.email = mixed_case_email
+	#		@user.save@user.reload.email.should == mixed_case_email.downcase
+	#	end
+	#end
+
+
   #pending "add some examples to (or delete) #{__FILE__}"
 
 end
